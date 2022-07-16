fda9c3c431f33c7d1ae887706397481d179a5a3d
TaherAhmed <TaherAhmed16@hotmail.com>
2022-07-16 20:21:42 +0100

    commit

--- SpaceApp.Assets/src/js/asteroid.js
+++ SpaceApp.Assets/src/js/asteroid.js
@@ -1,6 +1,5 @@
 ﻿import api from './api/fetch-api';
 import addSolarSystemObjects from './milkyway';
-import { asteroidTableData } from './data/asteroid';
 import addTabsEventListeners from './ui-functionality/tabs';
 //Make api call for individual asteroid and render 3d model
 //Table is rendered through .NET 
@@ -22,8 +21,7 @@ function renderAsteroid() {
 function generateAsteroidData(asteroid) {
     if (asteroid) {
         generateSpaceModel(asteroid);
-        appendApiDateToObject(asteroid);
-        renderDetailsTableMarkup();
+        renderDetailsTableMarkup(asteroid);
     }
 }
 
@@ -60,74 +58,18 @@ function generateSpaceModel(asteroid) {
     });
 }
 
-function appendApiDateToObject(asteroid) {
-    const orbitalData = asteroid["orbital_data"] ? asteroid["orbital_data"] : null;
-
-    if (orbitalData) {
-        const eccentricity = orbitalData["eccentricity"];
-        const semiMajorAxis = orbitalData["semi_major_axis"];
-        const perihelionDistance = orbitalData["perihelion_distance"];
-        const inclination = orbitalData["inclination"];
-        const ascendingNodeLongitude = orbitalData["ascending_node_longitude"];
-        const meanAnomaly = orbitalData["mean_anomaly"];
-        const perihelionTime = orbitalData["perihelion_time"];
-        const orbitalPeriod = orbitalData["orbital_period"];
-        const aphelionDistance = orbitalData["aphelion_distance"];
-
-        let orbitParameters = asteroidTableData.find(item => { return item.name == "Orbit Parameters" });
-
-        orbitParameters.rows[0]["eccentricity"].value = eccentricity;
-        orbitParameters.rows[1]["semiMajorAxis"].value = semiMajorAxis;
-        orbitParameters.rows[2]["perihelionDistance"].value = perihelionDistance;
-        orbitParameters.rows[3]["inclination"].value = inclination;
-        orbitParameters.rows[4]["ascendingNodeLongitude"].value = ascendingNodeLongitude;
-        orbitParameters.rows[5]["meanAnomaly"].value = meanAnomaly;
-        orbitParameters.rows[6]["timeOfPerihelionPassage"].value = perihelionTime;
-        orbitParameters.rows[7]["siderealOrbitalPeriod"].value = orbitalPeriod;
-        orbitParameters.rows[8]["meanMotion"].value = eccentricity;
-        orbitParameters.rows[9]["aphelionDistance"].value = aphelionDistance;
-    }
-
-    const absoluteMagnitudeH = asteroid["absolute_magnitude_h"] ? asteroid["absolute_magnitude_h"] : null;
-    const estimatedDiameter = asteroid["estimated_diameter"] ? asteroid["estimated_diameter"] : null;
-
-    if (absoluteMagnitudeH && estimatedDiameter) {
-        const estimatedDiameterMinKm = estimatedDiameter.kilometers?.estimated_diameter_min;
-        const estimatedDiameterMaxKm = estimatedDiameter.kilometers?.estimated_diameter_max;
-        const estimatedDiameterMinMeters = estimatedDiameter.meters?.estimated_diameter_min;
-        const estimatedDiameterMaxMeters = estimatedDiameter.meters?.estimated_diameter_max;
-        const estimatedDiameterMinMiles = estimatedDiameter.miles?.estimated_diameter_min;
-        const estimatedDiameterMaxMiles = estimatedDiameter.miles?.estimated_diameter_max;
-        const estimatedDiameterMinFeet = estimatedDiameter.feet?.estimated_diameter_min;
-        const estimatedDiameterMaxFeet = estimatedDiameter.feet?.estimated_diameter_max;
-
-        let physicalParameters = asteroidTableData.find(item => { return item.name == "Physical Parameters" });
-
-        physicalParameters.rows[0]["absoluteMagnitudeH"].value = absoluteMagnitudeH;
-        physicalParameters.rows[1]["estimatedDiameterMinKm"].value = estimatedDiameterMinKm;
-        physicalParameters.rows[2]["estimatedDiameterMaxKm"].value = estimatedDiameterMaxKm;
-        physicalParameters.rows[3]["estimatedDiameterMinMeters"].value = estimatedDiameterMinMeters;
-        physicalParameters.rows[4]["estimatedDiameterMaxMeters"].value = estimatedDiameterMaxMeters;
-        physicalParameters.rows[5]["estimatedDiameterMinMiles"].value = estimatedDiameterMinMiles;
-        physicalParameters.rows[6]["estimatedDiameterMaxMiles"].value = estimatedDiameterMaxMiles;
-        physicalParameters.rows[7]["estimatedDiameterMinFeet"].value = estimatedDiameterMinFeet;
-        physicalParameters.rows[8]["estimatedDiameterMaxFeet"].value = estimatedDiameterMaxFeet;
-
-    }
-}
-
-function renderDetailsTableMarkup() {
+function renderDetailsTableMarkup(asteroid) {
     const asteroidDetails = document.querySelector('#asteroid-details');
     if (asteroidDetails) {
-        const tabHeaderMarkup = generateTabs()
-        const tableRows = generateTables();
+        const tabs = generateTabs();
+        const tables = generateTables(asteroid);
 
         const markup = `
             <ul class="nav nav-tabs mb-3">
-                ${tabHeaderMarkup}
+                ${tabs}
             </ul>
             <div class="tab-content">
-                ${tableRows}
+                ${tables}
             </div>
         `;
 
@@ -137,81 +79,236 @@ function renderDetailsTableMarkup() {
 }
 
 function generateTabs() {
-    let markup = '';
+    return `
+        <li class="nav-item">
+            <a data-tabs-source="orbit-parameters" class="nav-link active">
+                <i class="mdi mdi-home-variant d-lg-none d-block mr-1"></i>
+                <span class="d-none d-lg-block">Orbit Parameters</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a data-tabs-source="physical-parameters" class="nav-link">
+                <i class="mdi mdi-account-circle d-lg-none d-block mr-1"></i>
+                <span class="d-none d-lg-block">Physical Parameters</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a data-tabs-source="close-approach-data" class="nav-link">
+                <i class="mdi mdi-account-circle d-lg-none d-block mr-1"></i>
+                <span class="d-none d-lg-block">Close Approach Data</span>
+            </a>
+        </li>
+    `;
+}
 
-    if (asteroidTableData) {
-        asteroidTableData.forEach((asteroidItem, index) => {
-            if (asteroidItem.name) {
-                markup += `
-                    <li class="nav-item">
-                        <a data-tabs-source="${asteroidItem.name.replaceAll(' ', '-').toLowerCase()}" class="nav-link ${index == 0 ? 'active' : ''}">
-                            <i class="mdi mdi-home-variant d-lg-none d-block mr-1"></i>
-                            <span class="d-none d-lg-block">${asteroidItem.name}</span>
-                        </a>
-                    </li>
-                    `;
-            }
-        })
+function generateTables(asteroid) {
+    let markup = '';
+    const orbitalData = asteroid["orbital_data"] ? asteroid["orbital_data"] : null;
+
+    if (orbitalData) {
+        markup += generateOrbitParamTable(orbitalData);
+    }
+
+    const absoluteMagnitudeH = asteroid["absolute_magnitude_h"] ? asteroid["absolute_magnitude_h"] : null;
+    const estimatedDiameter = asteroid["estimated_diameter"] ? asteroid["estimated_diameter"] : null;
+
+    if (absoluteMagnitudeH && estimatedDiameter) {
+        markup += generatePhysicalParamsTable({ 'absolute_magnitude_h': absoluteMagnitudeH, 'estimated_diameter': estimatedDiameter })
+    }
+
+    const closeApproachData = asteroid["close_approach_data"] ? asteroid["close_approach_data"] : null;
+
+    if (closeApproachData) {
+        markup += generateCloseApproachTable(closeApproachData);
     }
 
     return markup;
 }
 
-function generateTables() {
+function generateOrbitParamTable(orbitalData) {
     let markup = '';
 
-    asteroidTableData.forEach((item, index) => {
-        const tableRowHeaders = generateTableRowHeaders(item.headers);
-        const tableRows = generateTableRows(item.rows);
+    const eccentricity = orbitalData["eccentricity"];
+    const semiMajorAxis = orbitalData["semi_major_axis"];
+    const perihelionDistance = orbitalData["perihelion_distance"];
+    const inclination = orbitalData["inclination"];
+    const ascendingNodeLongitude = orbitalData["ascending_node_longitude"];
+    const meanAnomaly = orbitalData["mean_anomaly"];
+    const perihelionTime = orbitalData["perihelion_time"];
+    const orbitalPeriod = orbitalData["orbital_period"];
+    const meanMotion = orbitalData["mean_motion"];
+    const aphelionDistance = orbitalData["aphelion_distance"];
 
-        markup += `
-        <div class="tab-pane ${index == 0 ? 'active' : ''}" data-tabs-target="${item.name.replaceAll(' ', '-').toLowerCase()}">
+    markup += `
+        <div class="tab-pane active" data-tabs-target="orbit-parameters">
             <table class="table mb-0">
                 <thead>
                     <tr>
-                        ${tableRowHeaders}
+                        <th class="border-top-0" scope="col">Element</th>
+                        <th class="border-top-0" scope="col">Value</th>
                     </tr>
                 </thead>
                 <tbody>
-                    ${tableRows}
+                    <tr>
+                        <th scope="row">Eccentricity</th>
+                        <td>${eccentricity}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Semi-major axis</th>
+                        <td>${semiMajorAxis}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Perihelion distance</th>
+                        <td>${perihelionDistance}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Inclination</th>
+                        <td>${inclination}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Ascending node longitude</th>
+                        <td>${ascendingNodeLongitude}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Mean anomaly</th>
+                        <td>${meanAnomaly}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Time of perihelion passage</th>
+                        <td>${perihelionTime}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Sidereal orbital period</th>
+                        <td>${orbitalPeriod}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Mean motion</th>
+                        <td>${meanMotion}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Aphelion distance</th>
+                        <td>${aphelionDistance}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
-        `
-    });
+        `;
 
     return markup;
 }
 
-function generateTableRowHeaders(headers) {
-    let markup = ``;
+function generatePhysicalParamsTable(physicalData) {
+    let markup = '';
 
-    headers.forEach(header => {
-        markup += `
-            <th class="border-top-0" scope="col">${header}</th>
-        `
-    });
+    const absoluteMagnitudeH = physicalData["absolute_magnitude_h"];
+    const esimatedDiameter = physicalData["estimated_diameter"];
+    const estimatedDiameterMinKm = esimatedDiameter?.kilometers?.estimated_diameter_min;
+    const estimatedDiameterMaxKm = esimatedDiameter?.kilometers?.estimated_diameter_max;
+    const estimatedDiameterMinMeters = esimatedDiameter?.meters?.estimated_diameter_min;
+    const estimatedDiameterMaxMeters = esimatedDiameter?.meters?.estimated_diameter_max;
+    const estimatedDiameterMinMiles = esimatedDiameter?.miles?.estimated_diameter_min;
+    const estimatedDiameterMaxMiles = esimatedDiameter?.miles?.estimated_diameter_max;
+    const estimatedDiameterMinFeet = esimatedDiameter?.feet?.estimated_diameter_min;
+    const estimatedDiameterMaxFeet = esimatedDiameter?.feet?.estimated_diameter_max;
 
-    return markup;
-}
-
-function generateTableRows(rows) {
-    let markup = ``;
-
-    rows.forEach(row => {
-        for (var key in row) {
-            const value = row[key].value;
-            const label = row[key].label;
-
-            if (value && label) {
-                markup += `<tr>
-                    <th scope="row">${label}</th>
-                    <td>${value}</td>
+    markup += `
+    <div class="tab-pane" data-tabs-target="physical-parameters">
+        <table class="table mb-0">
+            <thead>
+                <tr>
+                    <th class="border-top-0" scope="col">Element</th>
+                    <th class="border-top-0" scope="col">Value</th>
                 </tr>
-                `;
-            }
-        }
-    });
+            </thead>
+            <tbody>
+            <tr>
+               <th scope="row">Absolute Magnitude [H]</th>
+               <td>${absoluteMagnitudeH}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Min (Km)</th>
+               <td>${estimatedDiameterMinKm}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Min (Km)</th>
+               <td>${estimatedDiameterMaxKm}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Min (M)</th>
+               <td>${estimatedDiameterMinMeters}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Max (M)</th>
+               <td>${estimatedDiameterMaxMeters}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Min (Mi)</th>
+               <td>${estimatedDiameterMinMiles}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Max (Mi)</th>
+               <td>${estimatedDiameterMaxMiles}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Min (Ft)</th>
+               <td>${estimatedDiameterMinFeet}</td>
+            </tr>
+            <tr>
+               <th scope="row">Estimated Diameter Max (Ft)</th>
+               <td>${estimatedDiameterMaxFeet}</td>
+            </tr>
+         </tbody>
+        </table>
+    </div>
+    `;
+
+    return markup;
+}
+
+function generateCloseApproachTable(closeApproachData) {
+    let markup = '';
+
+    const rows = generateCloseApproachTableRows(closeApproachData);
+
+    markup += `
+    <div class="tab-pane" data-tabs-target="close-approach-data">
+        <table class="table mb-0">
+            <thead>
+                <tr>
+                    <th class="border-top-0" scope="col">Close Approach Date</th>
+                    <th class="border-top-0" scope="col">Relative Velocity (Km/s)</th>
+                    <th class="border-top-0" scope="col">Miss Distance (Km)</th>
+                    <th class="border-top-0" scope="col">Orbiting Body</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${rows}
+         </tbody>
+        </table>
+    </div>
+    `;
+
+    return markup;
+}
+
+function generateCloseApproachTableRows(closeApproachData) {
+    let markup = '';
+
+    closeApproachData.forEach(item => {
+        const closeApproachDate = item["close_approach_date"];
+        const velocityKm = item["relative_velocity"]?.["kilometers_per_second"];
+        const missDistanceKm = item["miss_distance"]?.["kilometers"];
+        const orbitingBody = item["orbiting_body"];
+
+        markup += `
+            <tr>
+                <td>${closeApproachDate}</td>
+                <td>${velocityKm}</td>
+                <td>${missDistanceKm}</td>
+                <td>${orbitingBody}</td>
+            </tr>
+        `;
+    })
 
     return markup;
 }
